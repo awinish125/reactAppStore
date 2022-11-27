10c14505aaf4c8ea7c47b3c8f31406226f24abbc
awi-nish <104201978+awi-nish@users.noreply.github.com>
2022-11-27 19:40:26 +0530

    nkn

--- src/components/AppItem/index.js
+++ src/components/AppItem/index.js
@@ -3,6 +3,7 @@ const AppItem = props => {
   const {appName, imageUrl} = appList
   return (
     <li>
+      <h1>hii</h1>
       <img src={imageUrl} alt={appName} />
       <p>{appName}</p>
     </li>
